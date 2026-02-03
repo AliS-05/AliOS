436f1b6be8d6ce99cc7236b5277b94320e550e4c
Ali-Terminal <sefrali05@gmail.com>
2026-02-02 18:16:53 -0600

    Added isDigit(), print_num(), atoi (int to string), itoa (string to num), started on simple malloc().

--- extra.cpp
+++ extra.cpp
@@ -58,21 +58,80 @@ extern "C" void parse_command() {
     }
 }
 
+int isDigit(char digit){
+	if(digit >= '0'&& digit <= '9'){
+		return 1;
+	}
+	return 0;
+}
+
+int atoi(const char* str){
+	int res = 0;
+	while(*str == ' ') str++;
+	int sign = 1;
+	
+	if(*str == '-'){
+		sign = -1;
+		str++;
+	}
+	
+	while (isDigit(*str)) {
+		res = res * 10 + (*str - '0'); // considers digit place and adds ones place
+		str++;
+	}
+	return res * sign;
+}
+
+void itoa(int num, char* buf){
+	if (num == 0){
+		buf[0] = '0';
+		buf[1] = 0;
+		return;
+	}
+
+	int i = 0;
+	int is_neg = 0;
+	if(num < 0){
+		is_neg = 1;
+		num = -num;
+	}
+
+	while(num > 0){
+		buf[i++] = (num % 10) + '0'; //increments i
+		num /= 10;
+	}
+
+	if(is_neg) buf[i++] = '-';
+	buf[i] = 0;
+
+	for (int j = 0; j < i/2 ;j++){ // 
+		char temp = buf[j];
+		buf[j] = buf[i-1-j];
+		buf[i-1-j] = temp;
+	}
+}
+
+void print_num(int num){
+	char buf[12];
+	itoa(num, buf);
+	print_string(buf, cursor_pos);
+}
+
+char* heap_start = (char*)0x100000;
+char* heap_current =heap_start;
+
+void* malloc(int size){
+	void* ptr = heap_current;
+	heap_current += size;
+	return ptr;
+}
+
+
+
 extern "C" void kernel_main(){
 	print_string("BOOT OK", 0);
 	print_string(shell_prompt, 160);
-	//unsigned int addr = (unsigned int)shell_prompt;
-	//print_string("Address is: ", 0);
-	//// We can't print hex yet, so just test if shell_prompt is accessible
 
-	//// Test 2: Access first few characters directly
-	//char test[20];
-	//test[0] = shell_prompt[0];
-	//test[1] = shell_prompt[1];  
-	//test[2] = shell_prompt[2];
-	//test[3] = 0;
-	//print_string(test, 0);
-
-	//// Test 3: Try the original
+	print_num(1677);
 
 }

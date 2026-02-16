fe81ea50045d0ffe43fbb73108c61381a67332a9
AliS-05 <sefrali05@gmail.com>
2026-02-16 11:04:09 -0600

    started on codegen

--- assembler/src/assembler.c
+++ assembler/src/assembler.c
@@ -89,6 +89,7 @@ int main(int argc, char** argv) {
 	printSymbolTable(&table);
 	
 
+
 	free(tokenArray);
 
 	return 0;
---
+++ assembler/src/codegen.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//loop through instruction vector, translate instruction based on mnemonic register / immediate etc, look up labels in symbolTable,  emit code byte, 
+void startCodegen(){
+
+}
